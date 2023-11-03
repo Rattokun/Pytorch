3c8c70c64f7d8b4e4f7ec05abdd4caae56d60ec0
Rattokun <vitaliy-kuzin-00@mail.ru>
2023-11-03 17:18:38 +0300

    First commit

---
+++ image_processing/train.py
@@ -0,0 +1,29 @@
+from image_processing.download import trainloader
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+#Я возможно сглупил, но это же сохранение состояния как раз. Нужно ли ещё что-то дописывать или это оно и есть?
